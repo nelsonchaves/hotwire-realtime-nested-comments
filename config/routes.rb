4a87afa956203860c4683c893b12d8dae05517f2
Nelson Chaves <nelsonchavespro@gmail.com>
2021-02-09 17:14:21 -0800

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,24 @@
+require 'sidekiq/web'
+
+Rails.application.routes.draw do
+  resources :posts do
+    resources :comments, module: :posts
+  end
+
+  resources :comments do
+    resources :comments, module: :comments
+  end
+
+  get '/privacy', to: 'home#privacy'
+  get '/terms', to: 'home#terms'
+    authenticate :user, lambda { |u| u.admin? } do
+      mount Sidekiq::Web => '/sidekiq'
+    end
+
+
+  resources :notifications, only: [:index]
+  resources :announcements, only: [:index]
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  root to: 'home#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
